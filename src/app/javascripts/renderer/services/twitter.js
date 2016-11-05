9e2c18c57de2a5f736eb8e7c7ff09833d42048ea
田畑颯 <chinarra.tabata@gmail.com>
2016-11-06 00:42:50 +0900

    regist twitter api and access token

---
+++ src/app/javascripts/renderer/services/twitter.js
@@ -0,0 +1,8 @@
+const Twit = require('twit');
+
+module.exports = new Twit({
+   consumer_key:'4UQWPGI1g4V2HnnASne9yEtYE',
+   consumer_secret:'x2pTL2RnhUyDdx4kn2tNsbPXu4cugu84XnGoydkgbhvLcwJHhF',
+   access_token:'840439046-IoeWr4dLDNAc1sN0i8EJkj87Wms14PMCmX3U5jhI',
+   access_token_secret:'sWnMQloCIXNQQG1Zh27LHk3zBmG9G6ZyaaVwxFfryUnYE'
+});
