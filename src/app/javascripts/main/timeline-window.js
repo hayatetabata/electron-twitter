90922cdae85dca23c30f869b12d80522a390c8c1
田畑颯 <chinarra.tabata@gmail.com>
2017-02-11 11:22:34 +0900

    edit detail

--- src/app/javascripts/main/timeline-window.js
+++ src/app/javascripts/main/timeline-window.js
@@ -1,45 +1,48 @@
 const {app,BrowserWindow, ipcMain} = require('electron');
 
 module.exports = class TimelineWindow{
-   constructor(){
-      this.window = null;
-      this.newMentionsCount = 0;
-      this.start();
-   }
+    constructor(){
+    this.window = null;
+    this.newMentionsCount = 0;
+    this.start();
+    }
 
-   start(){
-      app.on('ready',() => {
-         this.createWindow(); 
-      });
+    start(){
+        app.on('ready',() => {
+            this.createWindow(); 
+        });
 
-      ipcMain.on('newMention', () => {
-        if (this.window.isFocused()){
-            return;
+        ipcMain.on('newMention', () => {
+            if (this.window.isFocused()){
+                return;
+            }
+
+            this.newMentionsCount++;
+            this.updateBadge();
+        });
+    }
+
+    createWindow(){
+        this.window = new BrowserWindow({
+            x:0,
+            y:0,
+            width:400,
+            height:600
+        });
+        this.window.on('focus', () => {
+            this.newMentionCount = 0;
+            this.updateBadge();
+        });
+
+        this.window.loadURL(
+          'file://'+ __dirname + '/../../html/main.html'
+        );
+    }
+
+    updateBadge() {
+        app.setBadgeCount(this.newMentionsCount);
+        if (process.platform === 'darwin') {
+            app.dock.bounce();
         }
-
-        this.newMentionsCount++;
-        this.updateBadge();
-      });
-   }
-
-   createWindow(){
-      this.window = new BrowserWindow({
-         x:0,
-         y:0,
-         width:400,
-         height:600
-      });
-      this.window.on('focus', () => {
-        this.newMentionCount = 0;
-        this.updateBadge();
-      });
-
-      this.window.loadURL(
-         'file://'+ __dirname + '/../../html/main.html'
-      );
-   }
-
-   updateBadge() {
-       app.setBadgeCount(this.newMentionsCount);
-   }
+    }
 };
